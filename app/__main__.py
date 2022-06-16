d104d9af52fb34cdb0f064da88f2d2d524bb36a6
duarte-pompeu <duarte-pompeu@users.noreply.github.com>
2022-06-16 23:48:50 +0100

    implement simple webserver

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,10 @@
 import config
+from webserver import WebServer
 
 config.setup()
 
 print("Hello, world!")
 print(f"Example from config: {config.settings.example}")
+
+server = WebServer()
+server.run()
---
+++ app/webserver.py
@@ -0,0 +1,25 @@
+from wsgiref.simple_server import make_server
+from wsgiref.util import setup_testing_defaults
+
+
+class WebServer:
+    def run(self):
+        # A relatively simple WSGI application. It's going to print out the
+        # environment dictionary after being updated by setup_testing_defaults
+        def simple_app(environ, start_response):
+            setup_testing_defaults(environ)
+
+            status = "200 OK"
+            headers = [("Content-type", "text/plain; charset=utf-8")]
+
+            start_response(status, headers)
+
+            ret = [
+                ("%s: %s\n" % (key, value)).encode("utf-8")
+                for key, value in environ.items()
+            ]
+            return ret
+
+        with make_server("", 8000, simple_app) as httpd:
+            print("Serving on port 8000...")
+            httpd.serve_forever()
